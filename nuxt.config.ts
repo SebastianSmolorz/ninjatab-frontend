d543685d5d75139fce807a9b620f8ccd9d935548
sebsmolorz <seb@zenstores.com>
2026-03-02 20:41:30 +0000

    Add app default favicon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt'],
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui', '@pinia/nuxt'],
 
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
 
-  colorMode: {
-    preference: 'dark'
-  },
+    colorMode: {
+        preference: 'dark'
+    },
 
-  runtimeConfig: {
-    public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api'
-    }
-  }
+    runtimeConfig: {
+        public: {
+            apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api'
+        }
+    },
+
+    app: {
+        head: {
+            title: 'Tab.ninja - Free app keep track of and settle shared costs',
+            htmlAttrs: {
+                lang: 'en',
+            },
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            ],
+        },
+    },
 })
