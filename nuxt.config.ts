2d9d7d0eb72ec0e710ca14908f281b9ee2ded250
sebsmolorz <seb@zenstores.com>
2026-05-23 23:43:44 +0100

    Add module to nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: false},
-    modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/fonts', '@vercel/analytics'],
+    modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/fonts', '@vercel/analytics', '@vercel/speed-insights'],
 
     fonts: {
         families: [
